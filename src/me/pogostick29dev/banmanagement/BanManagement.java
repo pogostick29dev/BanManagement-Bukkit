cd67a4287946ec1e71a9a0950161ed4e4c9e57b8
nrubin29 <nrubin29@gmail.com>
2015-03-25 17:35:47 -0400

    Implemented key security. Added config file. This version will not secure your server. Please wait for the next video.

--- src/me/pogostick29dev/banmanagement/BanManagement.java
+++ src/me/pogostick29dev/banmanagement/BanManagement.java
@@ -12,24 +12,36 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.Arrays;
 import java.util.UUID;
 
 public class BanManagement extends JavaPlugin implements Listener {
 
+    private String ip, key;
+
     @Override
     public void onEnable() {
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+
+        this.ip = getConfig().getString("ip");
+        this.key = getConfig().getString("key");
+
         Bukkit.getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+    public boolean onCommand(final CommandSender sender, Command cmd, String commandLabel, String[] args) {
         if (args.length == 0) {
             sender.sendMessage(ChatColor.RED + "Please enter the name or UUID of the player you would like to manage.");
             return true;
         }
 
-        String name, uuid;
+        final String name, uuid;
 
         if (args[0].matches("[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}")) {
             // They are typing in a UUID.
@@ -75,25 +87,84 @@ public class BanManagement extends JavaPlugin implements Listener {
                 msg = "You have been banned!";
             }
 
-            // TODO: Push the UUID, ban reason (msg), and time to the Web API.
+            try {
+                HttpURLConnection connection = connect("add", "uuid=" + uuid + "&reason=" + msg.replaceAll(" ", "%20"));
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String data = in.readLine();
 
-            sender.sendMessage(ChatColor.GREEN + "You have banned " + name + " for " + msg + "!");
+                if (data == null || data.equals("key")) {
+                    sender.sendMessage(ChatColor.RED + "BanManagement's config file is not configured properly.");
+                }
 
-            Player target = Bukkit.getServer().getPlayer(name);
+                else {
+                    sender.sendMessage(ChatColor.GREEN + "You have banned " + name + " for " + msg + "!");
 
-            if (target != null) {
-                target.kickPlayer(msg);
+                    Player target = Bukkit.getServer().getPlayer(name);
+
+                    if (target != null) {
+                        target.kickPlayer(msg);
+                    }
+                }
+            }
+
+            catch (FileNotFoundException e) {
+                sender.sendMessage(ChatColor.RED + "BanManagement's config file is not configured properly.");
+                return true;
+            }
+
+            catch (Exception e) {
+                e.printStackTrace();
             }
         }
 
         else if (cmd.getName().equalsIgnoreCase("unban")) {
-            // TODO: Push the UUID to the Web API.
+            try {
+                HttpURLConnection connection = connect("remove", "uuid=" + uuid);
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String data = in.readLine();
 
-            sender.sendMessage(ChatColor.GREEN + "You have unbanned " + name + "!");
+                if (data == null || data.equals("key")) {
+                    sender.sendMessage(ChatColor.RED + "BanManagement's config file is not configured properly.");
+                }
+
+                else {
+                    sender.sendMessage(ChatColor.GREEN + "You have unbanned " + name + "!");
+                }
+            }
+
+            catch (FileNotFoundException e) {
+                sender.sendMessage(ChatColor.RED + "BanManagement's config file is not configured properly.");
+                return true;
+            }
+
+            catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
         else if (cmd.getName().equalsIgnoreCase("checkban")) {
-            // TODO: Send a request to the Web API and read the data.
+            try {
+                HttpURLConnection connection = connect("checkban", "uuid=" + uuid);
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String data = in.readLine();
+
+                if (data == null || data.equals("key")) {
+                    sender.sendMessage(ChatColor.RED + "BanManagement's config file is not configured properly.");
+                }
+
+                else {
+                    sender.sendMessage(ChatColor.GREEN + data);
+                }
+            }
+
+            catch (FileNotFoundException e) {
+                sender.sendMessage(ChatColor.RED + "BanManagement's config file is not configured properly.");
+                return true;
+            }
+
+            catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
         return true;
@@ -101,6 +172,53 @@ public class BanManagement extends JavaPlugin implements Listener {
 
     @EventHandler
     public void onPlayerLogin(PlayerLoginEvent e) {
-        // TODO: Send a request to the Web API to see if the player is banned and why.
+        try {
+            HttpURLConnection connection = connect("get", "uuid=" + e.getPlayer().getUniqueId());
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String data = in.readLine();
+
+            if (data == null || data.equals("key")) {
+                e.disallow(PlayerLoginEvent.Result.KICK_BANNED, ChatColor.RED + "BanManagement's config file is not configured properly.");
+            }
+        }
+
+        catch (FileNotFoundException ex) {
+            e.disallow(PlayerLoginEvent.Result.KICK_BANNED, ChatColor.RED + "BanManagement's config file is not configured properly.");
+        }
+
+        catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    private HttpURLConnection connect(String api, String args) {
+        try {
+            args = "key=" + key + "&" + args;
+            byte[] data = args.getBytes(Charset.forName("UTF-8"));
+            int dataLength = args.length();
+            String request = "http://" + ip + "/api/" + api + ".php";
+            URL url = new URL(request);
+
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+            connection.setRequestProperty("charset", "UTF-8");
+            connection.setRequestProperty("Content-Length", String.valueOf(dataLength));
+
+            DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+            out.write(data);
+            out.flush();
+            out.close();
+
+            return connection;
+        }
+
+        catch (Exception e) {
+            e.printStackTrace();
+
+            return null;
+        }
     }
 }
